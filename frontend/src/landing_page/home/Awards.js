77984510673a6bc3c4b05726b0ceb7b9949b8ab8
Mohit <mohitbaghel1234321@gmail.com>
2026-06-20 17:41:02 +0530

    Awards Complete

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -2,7 +2,54 @@ import React from 'react';
 
 function Awards() {
     return ( 
-        <h1>Awards</h1>
+       <div className='container mb-5  '>
+        <div className='row'>
+            <div className='col-6'>
+                <img src='media/images/largestBroker.svg'></img>
+            </div>
+            <div className='col-6 p-5 mt-3'> 
+                <h2>Largest stock broker in India </h2>
+                <p className='mb-4'>
+                    2+ million zerodha clients contribute to ober 15% of all retail order values in india daily by trading and inversting in:
+                 <div className='row'>
+                      <div className='col-6 p-5 '>
+                        <ul>
+                            <li>
+                                Future and option
+                            </li>
+                            <li>
+                              Commodity derivatives
+                            </li>
+                            <li>
+                              Corrently derivates
+                            </li>
+                        </ul>
+                        
+                    </div>
+                    <div className='col-6 p-5'>
+                        <ul>
+                            <li>
+                                Future and option
+                            </li>
+                            <li>
+                              Commodity derivatives
+                            </li>
+                            <li>
+                              Corrently derivates
+                            </li>
+                        </ul>
+                    </div>
+                 </div>
+              
+                 <img src='media/images/pressLogos.png' style={{width:"95%"}}></img>
+                  
+                </p>
+
+
+            </div>
+        </div>
+
+       </div>
         
      );
 }
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 function Hero() {
     return ( 
-    <h1 > Hero <i class="fa-solid fa-user"></i> </h1>
+        <div className='container p-5 mb-5' >
+            <div className='row text-center'>
+                <img src='media/images/homeHEro.png' alt='Hero Image' ></img>
+                <h1 className='mt-5'>Invest in everything</h1>
+                <p className='mt-2'> Online platform to inverst in stoks , derivatatives,mutual finds and more </p>
+                <button  type="button" class="btn btn-primary p-2 mt-2 mb-5 fs-5 "style={{ width:"20%" , margin:"0 auto" }}>Signup now</button>
+            </div>
+
+
+        </div>
+    
+    
+
  );
 }
 
