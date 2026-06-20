04d50fdc9969bae31f581e985fa92f9e00c82537
Mohit <mohitbaghel1234321@gmail.com>
2026-06-20 18:26:09 +0530

    Start

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
 function Pricing() {
-    return ( 
-        <h1>
-          Pricing
-        </h1>
-     );
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6'>
+                    <h2>
+                        Unbeatable pricing
+                    </h2>
+                    <p>
+                        We pioneered the concept of discount broking and price
+                        transparency in India. Flat fee and no hidden charges.
+                    </p>
+                </div>
+                <div className='col-6'  >
+                  <div className='row' style={{border:"0.1px solid black"}}>
+                    <div className='col-6' style={{border:"0.1px solid black"}}>
+                      <h1>₹0</h1>
+                    </div>
+                    <div className='col-6 ' style={{border:"0.1px solid black"}}>
+                      <h1>₹20</h1>
+                    </div>
+                  </div>
+
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Pricing;
--- frontend/src/landing_page/home/States.js
+++ frontend/src/landing_page/home/States.js
@@ -2,9 +2,43 @@ import React from 'react';
 
 function States() {
     return ( 
-        <h1>
-           States
-        </h1>
+      <div className='container mt-5'>
+         <div className='row'>
+            <div className='col-6 p-2'>
+               <h2>
+                  Trust with confidence
+               </h2>
+
+               <br></br>
+
+               <h4 >Costomer-first always</h4>
+               <p>
+                  That's why 1.3+ caros customers tust Zerodha with ₹3.5+ lakh carots worth of equal investments
+               </p>
+               <h4>No spam or gimmicks</h4>
+               <p>
+                  That's why 1.3+ caros customers tust Zerodha with ₹3.5+ lakh carots worth of equal investments
+               </p>
+               <h4>the Zerodha Universe</h4>
+               <p>
+                  That's why 1.3+ caros customers tust Zerodha with ₹3.5+ lakh carots worth of equal investments
+               </p>
+               <h4>Do better with money</h4>
+               <p>
+                  That's why 1.3+ caros customers tust Zerodha with ₹3.5+ lakh carots worth of equal investments
+               </p>
+
+            </div>
+            <div className='col-6 p-2 '>
+               <img src='media\images\ecosystem.png' style={{width:"80%"}}></img>
+            </div>
+
+         </div>
+
+      </div>
+
+
+
      );
 }
 
