5a49b9f36987f0734bea0d4e0c3c34b1e540d9d7
Mohit <mohitbaghel1234321@gmail.com>
2026-06-24 13:41:09 +0530

    Complete-Support

--- frontend/src/landing_page/supports/CreateTicket.js
+++ frontend/src/landing_page/supports/CreateTicket.js
@@ -1,11 +1,167 @@
-import React from 'react';
+import React from "react";
 
 function CreateTicket() {
-    return ( 
-        <h1>
-            Hello world
-        </h1>
-     );
+  return (
+    <div className="container">
+      <div className="row mt-5">
+        <h3>To cretae a Ticket, Select a relevant topic</h3>
+        <div className="col-4 mt-5 p-2">
+          <h5 className="mb-5">
+            <i class="fa-solid fa-circle-plus"></i> Account Opening
+          </h5>
+          <a href="" className="text-decoration-none">
+            Online Account opening
+          </a>{" "}
+          <br />
+          <a href="" className="text-decoration-none">
+            Offline Account opening
+          </a>{" "}
+          <br />
+          <a href="" className="text-decoration-none">
+            Company, Partneship and HUF Account opening
+          </a>{" "}
+          <br />
+          <a href="" className="text-decoration-none">
+            NRI Account opening
+          </a>{" "}
+          <br />
+          <a href="" className="text-decoration-none">
+            Changes at Zerodha
+          </a>{" "}
+          <br />
+          <a href="" className="text-decoration-none">
+            Zerodha IDFC FIRST Bank 3-in-1 Account
+          </a>{" "}
+          <br />
+          <a href="" className="text-decoration-none">
+            Getting Started
+          </a>{" "}
+          <br />
+        </div>
+        <div className="col-4 mt-5 p-2">
+          <i class="fa-solid fa-circle-user"></i>Your Zerodha Account
+          <div className="mt-5">
+            <a href="" className="text-decoration-none">
+              Your Profile
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Account modification
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Client Master Report (CMR) and Depository Participant (DP)
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              NRI Account opening
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Nomination
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Zerodha IDFC FIRST Bank 3-in-1 Account
+            </a>{" "}
+            <br />
+          </div>
+        </div>
+        <div className="col-4 mt-5 p-2">
+          <i class="fa-solid fa-chart-line"></i>Kite
+          <div>
+            <a href="" className="text-decoration-none">
+              IPO
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Trading FAQs
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Margin Trading Facility (MTF) and Margins
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Charts and orders
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Alerts and Nudges
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              General
+            </a>{" "}
+            <br />
+          </div>
+        </div>
+
+        <div className="col-4 mt-5 p-2">
+          <h5>
+            <i class="fa-regular fa-credit-card"></i> Funds
+            <div>
+              <a href="" className="text-decoration-none">
+                Add money
+              </a>{" "}
+              <br />
+              <a href="" className="text-decoration-none">
+                Withdraw money
+              </a>{" "}
+              <br />
+              <a href="" className="text-decoration-none">
+                Add bank accounts
+              </a>{" "}
+              <br />
+              <a href="" className="text-decoration-none">
+                eMandates
+              </a>{" "}
+              <br />
+            </div>
+          </h5>
+        </div>
+        <div className="col-4 mt-5 p-2">
+          <i class="fa-solid fa-circle-notch"></i>Console
+          <div>
+            <a href="" className="text-decoration-none">
+              Portfolio
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Corporate actions
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Funds statement
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none"></a> <br />
+          </div>
+        </div>
+        <div className="col-4 mt-5 p-2">
+          <i class="fa-regular fa-circle"></i>Coin
+          <div>
+            <a href="" className="text-decoration-none">
+              Mutual funds
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Corporate actions
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Fixed Deposit (FD)
+            </a>{" "}
+            <br />
+            <a href="" className="text-decoration-none">
+              Features on Coin
+            </a>{" "}
+            <br />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- frontend/src/landing_page/supports/Hero.js
+++ frontend/src/landing_page/supports/Hero.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( 
-        <h1>
-            Hero
-        </h1>
-     );
+  return (
+    <div className="text-bg-info">
+      <div className="container">
+        <div className="row">
+          <div className="col-6 p-4">
+            <div className="text-light">
+              <h4>Support Portal </h4>
+            </div>
+            <div className="text-light mt-5">
+              <h3>
+                Search for an answer or browser help topics to create a ticket
+              </h3>
+              <div>
+                <input
+                  class="form-control form-control-lg"
+                  type="text"
+                  placeholder="Eg: how do i activate F&O ,why is my order getting rejected..."
+                  aria-label=".form-control-lg example"
+                ></input>
+              </div>
+              <div>
+                <a className="text-light" href="">
+                  Track account openeng
+                </a>  
+                <a className="text-light" href="">
+                  Track segment activation
+                </a>
+                <a className="text-light" href="">
+                  Intraday margins
+                </a>{" "}
+                <br />
+                <a className="text-light" href="">
+                  Kite user manual
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 mt-4 p-4 ">
+            <div className="text-end  ">
+              <a className="text-light"  href=""> Track Tickets</a>
+            </div>
+            <div>
+              <h3 className="text-light p-1 " >Fratured</h3>
+              <ol className="text-light">
+                <li >
+                  <a href="" className="text-light" >Current Takeovers and Delisting - january 2024</a>
+                </li>
+                <li>
+                  <a href="" className="text-light">Latest intraday leverages = MIS & CO</a>
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
