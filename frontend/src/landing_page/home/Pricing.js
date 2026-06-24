429e59e5be318a15a8a23ffac198307defd78834
Mohit <mohitbaghel1234321@gmail.com>
2026-06-24 14:38:26 +0530

    Home-Complete

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -1,41 +1,39 @@
-import React from 'react';
+import React from "react";
 
 function Pricing() {
-    return (
-        <div className='container mt-5'>
-            <div className='row'>  
-                <div className='col-6 mb-5'>
-                <div className='col-8'>
-                    <h2>
-                        Unbeatable pricing
-                    </h2>
-                    <p>
-                        We pioneered the concept of discount broking and price
-                        transparency in India. Flat fee and no hidden charges.
-                    </p>
-                    <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="#">
-                          See Pricing <i class="fa-solid fa-arrow-right"></i>
-                                 </a>
-
-                </div>
-                </div>
-                <div className='col-6 mb-5'  >
-                  <div className='row' style={{border:"0.1px solid black"}}>
-                    <div className='col-6 text-center' style={{border:"0.1px solid black"}}>
-                      <h1>₹0</h1>
-                      <p>Free equity delivery and direct multual funds</p>
-                    </div>
-                    <div className='col-6 text-center ' style={{border:"0.1px solid black"}}>
-                      <h1>₹20</h1>
-                      <p>Intraday and F&O</p>
-                    </div>
-                  </div>
-
-
-                </div>
-            </div>
+  return (
+    <div className="container ">
+      <div className="row">
+        <div className="col-4">
+          <div className="mb-5 fs-2">
+            <h1>Unbeatable pricing</h1>
+            <p>
+              We pioneered the concept of discount broking and price
+              transparency in India. Flat fee and no hidden charges.
+            </p>
+            <a href="" style={{ textDecoration: "none" }}>
+              See pricing <i class="fa-solid fa-arrow-right"></i>
+            </a>
+          </div>
         </div>
-    );
+        <div className="col-2"></div>
+        <div className="col-6 mb-5">
+          <div className="row text-center">
+            <div className="col p-3 border">
+              <h1 className="mb-3"> ₹0</h1>
+              <p>
+                Free wquity delivery and <br /> direct mutual funds
+              </p>
+            </div>
+            <div className="col p-3 border">
+              <h1 className="mb-3">₹20 </h1>
+              <p>Intraday and F&O</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
