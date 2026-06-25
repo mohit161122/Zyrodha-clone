8f8253c3d916ebcf0802c4a4305c9f9ffdd08283
Mohit <mohitbaghel1234321@gmail.com>
2026-06-25 14:28:26 +0530

    HEro

--- frontend/src/landing_page/supports/Hero.js
+++ frontend/src/landing_page/supports/Hero.js
@@ -3,33 +3,51 @@ import React from "react";
 function Hero() {
   return (
     <section className="container-fluid" id="supportHero">
-      <div className="p-5 " id="supportWrapper">
-        <h4 style={{ marginLeft: "150px" }}>Support Portal</h4>
-        <a href="" style={{ marginRight: "220px" }}>
+      <div className="p-5" id="supportWrapper">
+        <h4 style={{ marginLeft: "9.375rem" }}>Support Portal</h4>
+        <a href="" style={{ marginRight: "13.75rem" }}>
           Track Tickets
         </a>
       </div>
-      <div className="row p-0 m-3 ">
+
+      <div className="row p-0 m-3">
         <div className="col-6 p-3">
-          <h1 style={{ marginLeft: "280px" }} className="fs-3">
+          <h1 style={{ marginLeft: "17.5rem" }} className="fs-3">
             Search for an answer or browse help topics to <br /> create a ticket
           </h1>
+
           <input
-            style={{ marginLeft: "280px", width: "550px", height: "60px" }}
+            style={{
+              marginLeft: "17.5rem",
+              width: "34.375rem",
+              height: "3.75rem",
+            }}
             placeholder="Eg. how do I activate F&O"
           />
+
           <br />
           <br />
-          <div style={{ display: "flex", gap: "10px", marginLeft: "280px" }}>
+
+          <div
+            style={{
+              display: "flex",
+              gap: "0.625rem",
+              marginLeft: "17.5rem",
+            }}
+          >
             <a href="">Track account opening</a>
             <a href="">Track segment activation</a>
             <a href="">Intraday margins</a>
             <a href="">Kite user manual</a>
           </div>
         </div>
-        <div className="col-6 p-3" >
-          <h1 className="fs-3" style={{marginLeft:"70px"}}>Featured</h1>
-          <ol style={{marginLeft:"70px"}}>
+
+        <div className="col-6 p-3">
+          <h1 className="fs-3" style={{ marginLeft: "4.375rem" }}>
+            Featured
+          </h1>
+
+          <ol style={{ marginLeft: "4.375rem" }}>
             <li>
               <a href="">Current Takeovers and Delisting - January 2024</a>
             </li>
@@ -42,4 +60,5 @@ function Hero() {
     </section>
   );
 }
+
 export default Hero;
