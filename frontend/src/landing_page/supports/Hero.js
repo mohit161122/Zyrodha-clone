cc5cf2f4ec48dc7f858645b85cbcc02f620cc57d
Mohit <mohitbaghel1234321@gmail.com>
2026-06-25 14:22:59 +0530

    Support-PAge

--- frontend/src/landing_page/supports/Hero.js
+++ frontend/src/landing_page/supports/Hero.js
@@ -2,62 +2,44 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="text-bg-info">
-      <div className="container">
-        <div className="row">
-          <div className="col-6 p-4">
-            <div className="text-light">
-              <h4>Support Portal </h4>
-            </div>
-            <div className="text-light mt-5">
-              <h3>
-                Search for an answer or browser help topics to create a ticket
-              </h3>
-              <div>
-                <input
-                  class="form-control form-control-lg"
-                  type="text"
-                  placeholder="Eg: how do i activate F&O ,why is my order getting rejected..."
-                  aria-label=".form-control-lg example"
-                ></input>
-              </div>
-              <div>
-                <a className="text-light" href="">
-                  Track account openeng
-                </a>  
-                <a className="text-light" href="">
-                  Track segment activation
-                </a>
-                <a className="text-light" href="">
-                  Intraday margins
-                </a>{" "}
-                <br />
-                <a className="text-light" href="">
-                  Kite user manual
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col-6 mt-4 p-4 ">
-            <div className="text-end  ">
-              <a className="text-light"  href=""> Track Tickets</a>
-            </div>
-            <div>
-              <h3 className="text-light p-1 " >Fratured</h3>
-              <ol className="text-light">
-                <li >
-                  <a href="" className="text-light" >Current Takeovers and Delisting - january 2024</a>
-                </li>
-                <li>
-                  <a href="" className="text-light">Latest intraday leverages = MIS & CO</a>
-                </li>
-              </ol>
-            </div>
+    <section className="container-fluid" id="supportHero">
+      <div className="p-5 " id="supportWrapper">
+        <h4 style={{ marginLeft: "150px" }}>Support Portal</h4>
+        <a href="" style={{ marginRight: "220px" }}>
+          Track Tickets
+        </a>
+      </div>
+      <div className="row p-0 m-3 ">
+        <div className="col-6 p-3">
+          <h1 style={{ marginLeft: "280px" }} className="fs-3">
+            Search for an answer or browse help topics to <br /> create a ticket
+          </h1>
+          <input
+            style={{ marginLeft: "280px", width: "550px", height: "60px" }}
+            placeholder="Eg. how do I activate F&O"
+          />
+          <br />
+          <br />
+          <div style={{ display: "flex", gap: "10px", marginLeft: "280px" }}>
+            <a href="">Track account opening</a>
+            <a href="">Track segment activation</a>
+            <a href="">Intraday margins</a>
+            <a href="">Kite user manual</a>
           </div>
         </div>
+        <div className="col-6 p-3" >
+          <h1 className="fs-3" style={{marginLeft:"70px"}}>Featured</h1>
+          <ol style={{marginLeft:"70px"}}>
+            <li>
+              <a href="">Current Takeovers and Delisting - January 2024</a>
+            </li>
+            <li>
+              <a href="">Latest Intraday leverages - MIS & CO</a>
+            </li>
+          </ol>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
-
 export default Hero;
